058e12d9a5b6202b57491d60569fe2769dc5087c
Adesh000 <adeshnayak6@gmail.com>
2023-06-11 16:55:38 +0530

    added todo app

--- React/todo-app/src/App.js
+++ React/todo-app/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react';
+import "./App.css"
 
-function App() {
+const initialState = {
+  todos: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        todos: [...state.todos, action.payload],
+      };
+    case 'DELETE_TODO':
+      return {
+        todos: state.todos.filter(todo => todo.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+const TodoApp = () => {
+  const [todoText, setTodoText] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addTodo = () => {
+    if (todoText.trim() !== '') {
+      const newTodo = {
+        id: Date.now(),
+        text: todoText,
+      };
+      dispatch({ type: 'ADD_TODO', payload: newTodo });
+      setTodoText('');
+    }
+  };
+
+  const deleteTodo = id => {
+    dispatch({ type: 'DELETE_TODO', payload: id });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        placeholder="Enter a todo"
+        value={todoText}
+        onChange={e => setTodoText(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      
+      <ul>
+        {state.todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      
     </div>
   );
-}
+};
 
-export default App;
+export default TodoApp;
